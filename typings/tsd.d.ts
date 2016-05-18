9a3e23d039604f75253ffb4255987e008dfc238d
Janez Stupar <janez.stupar@visionect.si>
2016-05-18 22:06:15 +0200

    Fix bugs and make project run again.

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="nodeunit/nodeunit.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="glob/glob.d.ts" />
-/// <reference path="minimatch/minimatch.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
